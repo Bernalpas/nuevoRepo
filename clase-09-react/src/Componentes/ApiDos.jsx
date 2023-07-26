91947625debddf902249403e48e23830379327cb
Berni <bernalpas@gmail.com>
2023-07-26 17:06:29 -0300

    Props listo para deploy

--- clase-09-react/src/Componentes/ApiDos.jsx
+++ clase-09-react/src/Componentes/ApiDos.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button'
 import { Row, Col } from 'react-bootstrap'
+import Tarjetas from "./Tarjetas";
 
 
 
@@ -37,26 +38,15 @@ const ApiDos = () =>{
             <div className='text-center'>
                 <Button className='w-75 m-4' onClick={ imprimir }>Imprimir Datos en Consola</Button>
             </div>
-
-
-
-
             <h2 className='text-center m-4'>
                 Personajes de la Serie
             </h2>
 
                 <Row>
-                {personajes.map((personaje) => (
-                        <Col key={personaje.id}>
-                            <div class="card">
-                                <img src={personaje.image} class="card-img-top" alt="personaje" />
-                                <div class="card-body">
-                                    <h5 class="card-title">{personaje.name}</h5>
-                                    <p class="card-text">{personaje.species}</p>
-                                    <button class="btn btn-primary">Botón para algo..</button>
-                                </div>
-                            </div>
-                        </Col>
+                    {personajes.map((personaje) => (
+                            <Col key={personaje.id}>
+                                <Tarjetas personaje={personaje} />
+                            </Col>
                     ))}
                 </Row>
                 
